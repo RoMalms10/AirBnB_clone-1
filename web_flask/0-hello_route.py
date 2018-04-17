c5f2593dddc7d61a3e5f78281b02c2243047d75a
romalms10 <romalms10@gmail.com>
2018-04-17 02:12:00 +0000

    Write a simple Flask script
    Simple script that starts a Flask web application.
    The web application must be listening on 0.0.0.0 on port 5000.
    The route it will look for is '/' and will display the message "Hello HBNB!".
    The option strict_slashes=False is used in the route definition.

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" This module will import flask for a web application.
+    Web application needs to be waiting on 0.0.0.0 and port 5000.
+    The route is / and will call a function that displays "Hello HBNB!"
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_HBNB():
+    return "Hello HBNB!"
