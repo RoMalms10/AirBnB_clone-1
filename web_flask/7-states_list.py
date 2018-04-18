6e2a586f4ac808552bf7b706228e97c32452cf01
romalms10 <romalms10@gmail.com>
2018-04-18 21:59:07 +0000

    Create a Flask web application
    Has a route '/states_list'.
    The route gets the dictionary that includes all the STATE objects.
    Passes the dictionary to Jinja2.
    Jinja2 renders the template.

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Module that starts a Flask web application with routes and web templates.
+    - Uses FileStorage and DBStorage classes.
+    - One route is /states_list and calls a function that displays HTML page.
+"""
+
+from flask import Flask
+from flask import render_template
+import models
+
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def display_states_list_html():
+    """ Method that displays an HTML page that lists states """
+    state_dict = storage.all(models.classes["State"])
+    return render_template('7-states_list.html', state_dict
+
+
+@app.teardown_appcontext
+def close_session():
+    """ Method that closes the session """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
