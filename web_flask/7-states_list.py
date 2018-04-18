4eeea22ea2749702108a4c088bb7c1b94a48c2af
romalms10 <romalms10@gmail.com>
2018-04-18 22:52:02 +0000

    Add test

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -16,6 +16,7 @@ app = Flask(__name__)
 def display_states_list_html():
     """ Method that displays an HTML page that lists states """
     state_dict = storage.all(classes["State"])
+    print(state_dict)
     return render_template('7-states_list.html', state=state_dict)
 
 
