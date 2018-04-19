8af82e7225cc64627e19bb250c066f6c84e76dc2
romalms10 <romalms10@gmail.com>
2018-04-19 00:19:51 +0000

    Have if/else statement for db vs file storage

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -8,7 +8,7 @@
 from flask import Flask
 from flask import render_template
 from models import storage, classes
-
+import os
 
 app = Flask(__name__)
 
@@ -22,7 +22,10 @@ def display_cities_by_state_html():
     #Key is State obj, value is a list of city obj in that state
     state_and_city_dict = {}
     for key, value in state_dict.items():
-        state_and_city_dict[value] = value.cities()
+        if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+            state_and_city_dict[value] = value.cities
+        else:
+            state_and_city_dict[value] = value.cities()
     return render_template('8-cities_by_states.html', cs=state_and_city_dict)
 
 
